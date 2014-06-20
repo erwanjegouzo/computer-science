ccd7ba558a3a8a29c13bf25e06e6ecff043192db
Erwan Jegouzo <erwan.jegouzo@gmail.com>
2014-06-19 22:48:58 -0400

    binary search tree

--- binarysearchtree.js
+++ binarysearchtree.js
@@ -1,16 +1,3 @@
-function Node(data, left, right){
-	this.data = data;
-	this.left = left;
-	this.right = right;
-}
-
-Node.prototype = {
-	show: function(){
-		return this.data;
-	}
-}
-
-
 function BST(){
 	this.root = null;
 }
@@ -18,7 +5,12 @@ function BST(){
 BST.prototype = {
 
 	insert: function(data){
-		var n = new Node(data, null, null);
+		var n = {
+			data: data,
+			left: null,
+			right: null
+		};
+
 		if(this.root == null){
 			this.root = n;
 		}else{
@@ -49,11 +41,26 @@ BST.prototype = {
 	inOrder: function(node){
 		if(node){
 			this.inOrder(node.left);
-			console.log(node.show());
+			console.log(node.data);
 			this.inOrder(node.right);
 		}
-	}
+	},
 
+	preOrder: function(node){
+		if(node){
+			console.log(node.data);
+			this.preOrder(node.left);
+			this.preOrder(node.right);
+		}
+	},
+
+	postOrder: function(node){
+		if(node){
+			this.postOrder(node.left);
+			this.postOrder(node.right);
+			console.log(node.data);
+		}
+	}
 }
 
 var bst = new BST();
@@ -66,3 +73,6 @@ bst.insert(99);
 bst.insert(22);
 console.log('inOrder traversal');
 bst.inOrder(bst.root);
+
+//console.log('preOrder traversal');
+//bst.preOrder(bst.root);
